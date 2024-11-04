ec1014f9a75017705a3933b81d0b19e546e518ec
dramirez <dramirezrrzz@gmail.com>
2024-11-04 12:42:32 -0600

    Productos cargados al carrito la cantidad necesaria, y fin del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,47 @@ function App() {
 
   const [carrito, setCarrito] = useState([]);
 
+  const agregarProductoAlCarrito = (idProductoAgregar, nombre) => {
+    // si el cartito esta vacio
+    if (carrito.length === 0) {
+      setCarrito([{ id: idProductoAgregar, nombre: nombre, cantidad: 1 }]);
+    } else {
+      // revisar que el producto no exita
+      // si ya lo tiene actualizar la cantidad
+      // si no tiene el producto entonces se agrega
+      // para poder editar el arreglo se tiene que clonar
+      const nuevoCarrito = [...carrito];
+      // comprobar si el carrito ya tiene el ID del producto a agregar
+      const yaEstaEnCarrito =
+        nuevoCarrito.filter((prductoDeCarrito) => {
+          return prductoDeCarrito.id === idProductoAgregar;
+        }).length > 0;
+      // si ya tiene el producto entonces lo actualizamos
+      if (yaEstaEnCarrito) {
+        // para ello tenemos que burcarlo, obtener su posicion en el arreglo
+        // y en base a su posicion ya actualizamos el valor
+        nuevoCarrito.forEach((prductoDeCarrito, index) => {
+          if (prductoDeCarrito.id === idProductoAgregar) {
+            const cantidad = nuevoCarrito[index].cantidad;
+            nuevoCarrito[index] = {
+              id: idProductoAgregar,
+              nombre: nombre,
+              cantidad: cantidad + 1,
+            };
+          }
+        });
+      } else {
+        nuevoCarrito.push({
+          id: idProductoAgregar,
+          nombre: nombre,
+          cantidad: 1,
+        });
+      }
+      // actualizar el carrito
+      setCarrito(nuevoCarrito);
+    }
+  };
+
   return (
     <Contenedor>
       <Menu>
@@ -30,7 +71,15 @@ function App() {
         <Route path="*" element={<Error404 />} />
         <Route path="/" element={<Inicio />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/tienda" element={<Tienda productos={productos} />} />
+        <Route
+          path="/tienda"
+          element={
+            <Tienda
+              productos={productos}
+              agregarProductoAlCarrito={agregarProductoAlCarrito}
+            />
+          }
+        />
       </Routes>
       <main>
         <aside>
